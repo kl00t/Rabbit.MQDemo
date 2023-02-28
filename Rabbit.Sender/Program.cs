eb7fc77cbecd7b9465626bbb18ce7aad030b741f
Scott Vaughan <scott.vaughan@ensono.com>
2023-02-28 10:12:25 +0000

    Add project files.

---
+++ Rabbit.Sender/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new()
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672"),
+    ClientProvidedName = "Rabbit Sender App"
+};
+
+IConnection connection = factory.CreateConnection();
+
+IModel channel = connection.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "DemoQueue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello World!");
